307d8c56729b10eb36416fc5c7d732d8e6dcc017
minnjung0887921 <minjeong0887921@gmail.com>
2024-08-18 11:12:23 +0900

    지연로딩

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,9 +2,6 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -15,20 +12,30 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
             Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
-
+            member.setUsername("member1");
+            member.setTeam(team);
             em.persist(member);
 
             em.flush();
             em.clear();
 
+            Member m = em.find(Member.class, member.getId());
+
+            System.out.println("m = " + m.getTeam().getClass());
+
+            System.out.println("==========================");
+            m.getTeam().getName(); // 초기화
+            System.out.println("==========================");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
